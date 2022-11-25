93e45c7a500e0db5a43745d62a4921ba83f50ac3
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2022-11-25 09:05:34 +0100

    complete the code of regex and update's files

--- src/useRegex.py
+++ src/useRegex.py
@@ -1,1 +1,25 @@
+import requests
+import json
+url = "https://data.mongodb-api.com/app/data-ipwwl/endpoint/data/v1/action/find"
 
+payload = json.dumps({
+    "collection": "bikes",
+    "database": "bikes",
+    "dataSource": "Sandbox",
+    "filter": {"brand": {"$regex":"^b", "$options": "i"}}
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Request-Headers': '*',
+  'api-key': 'qR8ioaiwVwnYxVUg75DzUT92yGkhQcSqbWr2xGO6dwVekV2hhbin8iP44XC0JQFW'
+}
+
+parametros = {'clave1': 'valor1', 'clave2': 'valor2'}
+response = requests.request("POST", url, headers=headers, data=payload, params= parametros)
+contador = 0
+hola = response.text
+diccionario = json.loads(hola)
+consulta = diccionario['documents']
+for item in consulta:
+    print (item)
+    contador = contador + 1
--- src/useUpdate.py
+++ src/useUpdate.py
@@ -1,1 +1,27 @@
+import requests
+import json
 
+fieldValue = 'Bicicletas'
+updateValue = 'Biciclesssstas'
+
+url = "https://data.mongodb-api.com/app/data-ipwwl/endpoint/data/v1/action/updateMany"
+
+payload = json.dumps({
+    "collection": "bikes",
+    "database": "bikes",
+    "dataSource": "Sandbox",
+    "filter": {"brand":fieldValue},
+    "update":{
+        "$set": {
+            "brand": updateValue
+        }
+    }
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Request-Headers': '*',
+  'api-key': 'qR8ioaiwVwnYxVUg75DzUT92yGkhQcSqbWr2xGO6dwVekV2hhbin8iP44XC0JQFW'
+}
+
+
+response = requests.request("POST", url, headers=headers, data=payload)
